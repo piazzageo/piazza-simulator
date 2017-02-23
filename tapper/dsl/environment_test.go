49f951939cd32611d455dffd6edaaa11fe8e7c0b
mpg <mpg@flaxen.com>
2017-02-22 20:01:21 -0500

    partial restoration

--- tapper/dsl/environment_test.go
+++ tapper/dsl/environment_test.go
@@ -1,25 +1,26 @@
 package dsl
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 //---------------------------------------------------------------------------
 
 func TestEnvironment(t *testing.T) {
-	/*
-		assert := assert.New(t)
+	assert := assert.New(t)
 
-		// setup
+	// setup
+	tt, err := NewTypeTable()
+	assert.NoError(err)
+	err = tt.addNode("i", NewTypeNodeInt())
+	assert.NoError(err)
+	err = tt.addNode("f", NewTypeNodeFloat())
+	assert.NoError(err)
 
-		tt, err := NewTypeTable()
-		assert.NoError(err)
-		err = tt.addNode("i", NewTypeNodeInt())
-		assert.NoError(err)
-		err = tt.addNode("f", NewTypeNodeFloat())
-		assert.NoError(err)
+	env := NewEnvironment(tt)
 
-		env := NewEnvironment(tt)
-
-		env.set("i", &ExprValue{Type: IntType, Value: 12})
-		assert.Equal(ExprValue{Type: IntType, Value: 12}, env.get("i"))
-	*/
+	env.set("i", &ExprValue{Type: IntType, Value: 12})
+	assert.Equal(ExprValue{Type: IntType, Value: 12}, env.get("i"))
 }
--- tapper/dsl/eval_test.go
+++ tapper/dsl/eval_test.go
@@ -1,58 +1,61 @@
 package dsl
 
-import "testing"
+import (
+	"testing"
 
-type evalTestItem struct {
-	node   ExprNode
-	values map[string]*ExprValue
-	typs   map[string]TypeNode
-	result interface{}
-}
-
-var evalTestData = []evalTestItem{
-	evalTestItem{ // "a * (b + c )"
-		node: NewExprNodeMultiply(
-			NewExprNodeAdd(NewExprNodeSymbolRef("c"), NewExprNodeSymbolRef("b")),
-			NewExprNodeSymbolRef("a")),
-		values: map[string]*ExprValue{
-			"a": &ExprValue{Type: IntType, Value: 2},
-			"b": &ExprValue{Type: IntType, Value: 3},
-			"c": &ExprValue{Type: IntType, Value: 4},
-		},
-		typs: map[string]TypeNode{
-			"a": NewTypeNodeInt(),
-			"b": NewTypeNodeInt(),
-			"c": NewTypeNodeInt(),
-		},
-		result: 14,
-	},
-}
+	"github.com/stretchr/testify/assert"
+)
 
 func TestEval(t *testing.T) {
-	/*
-		assert := assert.New(t)
 
-		for _, item := range evalTestData {
+	assert := assert.New(t)
 
-			typeTable, err := NewTypeTable()
+	testData := []struct {
+		node   ExprNode
+		values map[string]ExprValue
+		typs   map[string]TypeNode
+		result ExprValue
+	}{
+		{ // "a * (b + c )"
+			node: NewExprNodeMultiply(
+				NewExprNodeAdd(NewExprNodeSymbolRef("c"), NewExprNodeSymbolRef("b")),
+				NewExprNodeSymbolRef("a")),
+			values: map[string]ExprValue{
+				"a": ExprValue{Type: IntType, Value: 2},
+				"b": ExprValue{Type: IntType, Value: 3},
+				"c": ExprValue{Type: IntType, Value: 4},
+			},
+			typs: map[string]TypeNode{
+				"a": NewTypeNodeInt(),
+				"b": NewTypeNodeInt(),
+				"c": NewTypeNodeInt(),
+			},
+			result: ExprValue{Type: IntType, Value: 14},
+		},
+	}
+
+	for _, item := range testData {
+
+		typeTable, err := NewTypeTable()
+		assert.NoError(err)
+
+		env := NewEnvironment()
+
+		err = typeTable.addStruct("x", nil)
+		for k, v := range item.typs {
+			err = typeTable.addField("x", FieldName(k), v)
 			assert.NoError(err)
-
-			env := NewEnvironment(typeTable)
-
-			for k, v := range item.typs {
-				err = typeTable.addNode(k, v)
-				assert.NoError(err)
-			}
-
-			for k, v := range item.values {
-				env.set(k, v)
-			}
-
-			eval := &Eval{}
-			_, err = eval.Evaluate(item.node, env)
-			assert.NoError(err)
-			//assert.NotNil(result)
-			//assert.EqualValues(item.result, result)
 		}
-	*/
+
+		for k, v := range item.values {
+			env.set(k, v)
+		}
+
+		eval := &Eval{}
+		result, err := eval.Evaluate(item.node, env)
+		assert.NoError(err)
+		assert.NotNil(result)
+		assert.EqualValues(item.result, result)
+	}
+
 }
